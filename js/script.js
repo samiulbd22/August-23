dfbc0e636dab364f3e7c63f183192aa3caa808c7
samiulbd22 <97846282+samiulbd22@users.noreply.github.com>
2023-08-11 18:27:19 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ const members = {
         garage_cost     :0,
         running_payable :3000,
         running_paid    :5000,
-        running_meal    :8,
+        running_meal    :10,
         // method calling
         previousAddedDining,
         previousDue,
@@ -40,8 +40,8 @@ const members = {
         stay_cost       :2330,
         garage_cost     :0,
         running_payable :3053,
-        running_paid    :0,
-        running_meal    :18,
+        running_paid    :5000,
+        running_meal    :22,
         // method calling
         previousAddedDining,
         previousDue,
@@ -67,7 +67,7 @@ const members = {
         garage_cost     :0,
         running_payable :4775,
         running_paid    :7000,
-        running_meal    :11,
+        running_meal    :12,
         // method calling
         previousAddedDining,
         previousDue,
@@ -91,8 +91,8 @@ const members = {
         stay_cost       :2000,
         garage_cost     :0,
         running_payable :2742,
-        running_paid    :0,
-	running_meal    :9,
+        running_paid    :2000,
+	running_meal    :10,
         // method calling
         previousAddedDining,
         previousDue,
@@ -195,7 +195,7 @@ const members = {
         garage_cost     :500,
         running_payable :4191,
         running_paid    :3000,
-        running_meal    :17,
+        running_meal    :19,
         // method calling
         previousAddedDining,
         previousDue,
@@ -221,7 +221,7 @@ const members = {
         garage_cost     :0,
         running_payable :2663,
         running_paid    :0,
-        running_meal    :19,
+        running_meal    :21,
         // method calling
         previousAddedDining,
         previousDue,

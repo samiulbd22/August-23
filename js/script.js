1051ea4f859cb1ed7160047f8c25d0ac01ffa696
samiulbd22 <97846282+samiulbd22@users.noreply.github.com>
2023-08-31 12:13:04 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const members = {
     serviceBill     :35,
     othersBill      :0,
     runningMonth    :"August-23",
-    runningMealRate :61,
+    runningMealRate :56,
 
     "01814843266":{
         name:"Istiaq Ahmad Udoy",
@@ -16,7 +16,7 @@ const members = {
         garage_cost     :0,
         running_payable :3000,
         running_paid    :5000,
-        running_meal    :38,
+        running_meal    :39,
         // method calling
         previousAddedDining,
         previousDue,
@@ -41,7 +41,7 @@ const members = {
         garage_cost     :0,
         running_payable :3053,
         running_paid    :5000,
-        running_meal    :50,
+        running_meal    :52,
         // method calling
         previousAddedDining,
         previousDue,
@@ -92,7 +92,7 @@ const members = {
         garage_cost     :0,
         running_payable :2742,
         running_paid    :3800,
-	running_meal    :24,
+	running_meal    :25,
         // method calling
         previousAddedDining,
         previousDue,
@@ -118,7 +118,7 @@ const members = {
         garage_cost     :0,
         running_payable :2803,
         running_paid    :6500,
-        running_meal    :37,
+        running_meal    :38,
         // method calling
         previousAddedDining,
         previousDue,
@@ -195,7 +195,7 @@ const members = {
         garage_cost     :500,
         running_payable :4191,
         running_paid    :5500,
-        running_meal    :21,
+        running_meal    :25,
         // method calling
         previousAddedDining,
         previousDue,
@@ -221,7 +221,7 @@ const members = {
         garage_cost     :0,
         running_payable :2663,
         running_paid    :0,
-        running_meal    :54,
+        running_meal    :55,
         // method calling
         previousAddedDining,
         previousDue,
